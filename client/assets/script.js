97cd8203b7f924f2cdc62cd55c54d43d5fc7d5ab
Stefan Mircea <stefancmircea@gmail.com>
2022-08-24 17:48:54 +0100

    functional search

--- client/assets/script.js
+++ client/assets/script.js
@@ -1,22 +1,32 @@
+const query = document.querySelector('#query')
+
 document.getElementById("button1").onclick = function (e) {
-        e.preventDefault()
-        location.href = "http://localhost:8000/results.html?";
-    };
+  e.preventDefault()
+  if (!query.value) {
+    location.href = "http://localhost:8000/results.html?";
+  } else {
+    window.open('https://www.google.com/search?q='+query.value)
+  }
+};
 
 
     
-    const random = async (e) => {
-      try{
-          const res = await fetch (`http://localhost:4000/ten-websites/random`);
-          const data = await res.text();
-          location.href = data;
-    
-          
-          
-      }
-      catch (error){
-          console.log(error)
-      }
-    }
+const random = async (e) => {
+  try{
+      const res = await fetch (`http://localhost:4000/ten-websites/random`);
+      const data = await res.text();
+      location.href = data;
 
-  document.getElementById("button2").onclick = random() 
+      
+      
+  }
+  catch (error){
+      console.log(error)
+  }
+}
+
+document.getElementById("button2").onclick = function (e) {
+  e.preventDefault()
+  random()
+}
+    

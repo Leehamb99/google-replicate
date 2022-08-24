73ba563eb12a63edf6beb14c5c1a8d83a1ef2c37
Leehamb99 <leehamb99@gmail.com>
2022-08-24 14:16:48 +0100

    add styling

--- client/assets/script.js
+++ client/assets/script.js
@@ -3,7 +3,22 @@
         location.href = "http://localhost:8000/results.html?";
     };
 
-  document.getElementById("button1").onclick = function (e) {
+
+    
+    const random = async (e) => {
+      try{
+          const res = await fetch (`http://localhost:4000/ten-websites/random`);
+          const data = await res.text();
+          location.href = data;
+    
+          
+          
+      }
+      catch (error){
+          console.log(error)
+      }
+    }
+
+  document.getElementById("button2").onclick = function (e) {
       e.preventDefault()
-      location.href = "";
   };

c5ebb8b021013245631cf93b7f89312370d8420c
Stefan Mircea <stefancmircea@gmail.com>
2022-08-24 14:10:46 +0100

    add results page

--- client/assets/script.js
+++ client/assets/script.js
@@ -1,21 +1,21 @@
-const button = document.querySelector("#button1");
+// const button = document.querySelector("#button1");
 
 
-const search = async (e) => {
-  try{
-      const res = await fetch (`http://localhost:4000/ten-websites`);
-      const data = await res.json();
-      data.forEach(element => {
-        document.getElementById('text1').innerHTML = element.title
-      });   
+// const search = async (e) => {
+//   try{
+//       const res = await fetch (`http://localhost:4000/ten-websites`);
+//       const data = await res.json();
+//       data.forEach(element => {
+//         document.getElementById('text1').innerHTML = element.title
+//       });   
 
       
       
-  }
-  catch (error){
-      console.log(error)
-  }
-}
+//   }
+//   catch (error){
+//       console.log(error)
+//   }
+// }
 
 
-module.exports = search
+// module.exports = search
